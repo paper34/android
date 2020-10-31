ae463f6c75abbefaeba0d6e0a2496733a54bbd0c
paper34 <69791408+paper34@users.noreply.github.com>
2020-10-31 18:37:30 +0900

    Add files via upload

---
+++ java08/src/클래스만들기/강아지.java
@@ -0,0 +1,14 @@
+package 클래스만들기;
+
+public class 강아지 {
+	String color;
+	String species;
+
+	public void shout() {
+		System.out.println("짖다");
+	}
+
+	public void bites() {
+		System.out.println("물다");
+	}
+}
