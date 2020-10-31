90b70ac3c88e189a7f0db6a149c02f2010695c90
paper34 <69791408+paper34@users.noreply.github.com>
2020-10-31 18:54:15 +0900

    Add files via upload

---
+++ java1213/src/컬럭션/로또번호생성중복제거.java
@@ -0,0 +1,20 @@
+package 컬럭션;
+
+import java.util.HashSet;
+import java.util.Random;
+
+public class 로또번호생성중복제거 {
+
+	public static void main(String[] args) {
+
+		Random r = new Random();
+		HashSet h = new HashSet();
+
+		while (h.size() < 6) {
+			h.add(r.nextInt(45) + 1);
+
+		}
+		System.out.println(h);
+	}
+
+}
