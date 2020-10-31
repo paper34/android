e8d0f2e7732cd9a0d2b1dcd4301ae6e261f748c4
paper34 <69791408+paper34@users.noreply.github.com>
2020-10-31 18:50:24 +0900

    Add files via upload

---
+++ java48/src/추상클래스/Animal.java
@@ -0,0 +1,9 @@
+package 추상클래스;
+
+public interface Animal {
+
+	void eat();
+
+	void sleep();
+
+}
---
+++ java49/src/인터페이스/맨.java
@@ -0,0 +1,11 @@
+package 인터페이스;
+
+public class 맨 {
+	public void run() {
+		System.out.println("빨리 다리다.");
+	}
+
+	public void eat() {
+		System.out.println("많이먹다");
+	}
+}
