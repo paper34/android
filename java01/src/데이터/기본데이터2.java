80ab072bfe14bbad849c2572b3d1a5f90c7ac1b2
paper34 <69791408+paper34@users.noreply.github.com>
2020-10-31 18:30:40 +0900

    Add files via upload

---
+++ java01/src/데이터/기본데이터2.java
@@ -0,0 +1,20 @@
+package 데이터;
+
+public class 기본데이터2 {
+
+	public static void main(String[] args) {
+		// 기본 데이터 4개지: 정수, 실수, 문자, 논리
+		// 기호 => 연산자
+		int age = 100; // 정수
+		double height = 122.2; // 실수
+		char gender = '남'; // 문자
+		boolean food = true;// false
+		System.out.println("오늘은 월요입니다.");
+		System.out.println("내 나이는:" + age);
+		System.out.println("내 키는:" + height);
+		System.out.println("내 성별은:" + gender);
+		System.out.println("점심은:" + food);
+		// 글자들: 문자+문자+문자+..... (여러글자, 문자열, string, 실)
+	}
+
+}
